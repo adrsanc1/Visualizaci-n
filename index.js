a8e3be59d3772a276241a7b4f366730795c76af3
adrsanc1 <130184435+adrsanc1@users.noreply.github.com>
2023-06-30 19:50:30 +0200

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -3,3 +3,39 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
+
+// Cargar los datos del archivo CSV
+d3.csv("./Carrefour_cleaning.csv", function(error, data) {
+  if (error) throw error;
+
+  // Convertir la columna 'order_date' a tipo 'date'
+  data.forEach(function(d) {
+    d.order_date = new Date(d.order_date + '-01');
+  });
+
+  // Configurar el gráfico interactivo con D3plus
+  var visualization = d3plus
+    .viz()
+    .container("#chart")
+    .data(data)
+    .type("line")
+    .id("category")
+    .x("order_date")
+    .y("value")
+    .time("order_date")
+    .title("Cantidad de productos vendidos por Categorías y Fecha")
+    .xLabel("Fecha")
+    .yLabel("Conteo")
+    .legend(true)
+    .ui([
+      {
+        method: "type",
+        value: ["line", "scatter"],
+        label: ["Línea", "Puntos"]
+      }
+    ])
+    .draw();
+});
+
+
+
